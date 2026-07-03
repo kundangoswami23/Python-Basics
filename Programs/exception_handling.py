fb72beb912a692f5fff97849dfb3f4e7754efafc
Kundan Goswami <kundangoswami1113@gmail.com>
2026-07-03 14:50:35 +0530

    Update exception_handling.py

--- Programs/exception_handling.py
+++ Programs/exception_handling.py
@@ -1,1 +1,11 @@
+try:
+    a = int(input("Enter Number : "))
+    b = int(input("Enter Number : "))
 
+    print(a/b)
+
+except ZeroDivisionError:
+    print("Cannot divide by zero")
+
+except ValueError:
+    print("Invalid Input")
